a338164a834055c613324e8a6f9e9dbca7b24698
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-06-21 12:31:24 +0800

    sm8750: Configure compression_with_xor.mk
    * getprop | grep virtual_ab
    [ro.virtual_ab.batch_writes]: [true]
    [ro.virtual_ab.compression.enabled]: [true]
    [ro.virtual_ab.compression.threads]: [true]
    [ro.virtual_ab.compression.xor.enabled]: [true]
    [ro.virtual_ab.enabled]: [true]
    [ro.virtual_ab.io_uring.enabled]: [true]
    [ro.virtual_ab.userspace.snapshots.enabled]: [true]

--- device.mk
+++ device.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Configure core_64_bit_only.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-# Configure virtual_ab compression.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+# Configure virtual_ab_ota compression_with_xor.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
 
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Configure twrp common.mk
+# Configure twrp config common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # API
